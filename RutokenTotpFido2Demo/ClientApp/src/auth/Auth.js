096f3d844c5a7ed129b3dd383dd01eae48945aeb
Lytkin Vladimir <lytkin.vs@hotmail.com>
2023-08-18 19:15:21 +0500

    main: Add logo on auth page
    Resolve: WEB-3816

--- RutokenTotpFido2Demo/ClientApp/src/auth/Auth.js
+++ RutokenTotpFido2Demo/ClientApp/src/auth/Auth.js
@@ -13,11 +13,23 @@ const Auth = () => {
 
     return (
         <div className="register-background">
-            <div className="container">
-                <div className="d-flex flex-row justify-content-center align-items-center vh-100">
-                    <AuthRenderer twoFactorType={twoFactorType}></AuthRenderer>
+            <div className="layout-container layout-container--auth">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-2">
+                            <div className='layout-logo layout-logo--auth'></div>
+                        </div>
+                        <div className="col-lg-8">
+                            <div className="d-flex flex-row justify-content-center align-items-center vh-100">
+                                <AuthRenderer twoFactorType={twoFactorType}></AuthRenderer>
+                            </div>
+                        </div>
+                        <div className="col-lg-2">
+                        </div>
+                    </div>
                 </div>
             </div>
+
         </div>
     );
 
